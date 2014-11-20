91c178d3bf91f8e6f4a77c8a30a5c99e24a260d7
gui2one <guillaume.rouault001@orange.fr>
2014-11-20 09:19:14 +0100

    morning

--- planet.js
+++ planet.js
@@ -15,9 +15,15 @@ function planet(_sceneObject,radius){
 	this.radius = radius;
 	this.scene = _sceneObject;
 	this.texture = new THREE.ImageUtils.loadTexture("maps/earth.jpg");
+	this.orbitRadius = 5;
+	this.orbitSpeed = 10;
 
 	this.material;
 	this.moon;
+	this.ring;
+	this.bDoRing = false;
+
+	moonsSpeedMullt = 0.01;
 
 	
 	this.setRadius = function(_radius){
@@ -26,9 +32,35 @@ function planet(_sceneObject,radius){
 		
 	}
 
-	this.setTexture= function(_texture){
-		this.texture = _texture;
+	this.setOrbitRadius = function(_orbitRadius){
 
+		this.orbitRadius = _orbitRadius;
+	}
+
+	this.setOrbitSpeed = function(_speed){
+
+		this.orbitSpeed = _speed;
+	}
+
+	this.setTexture= function(_texture){
+		this.material.map = _texture;
+
+	}
+
+	this.doRing = function(){
+
+		this.bDoRing = true;
+
+		var ringGeo = new THREE.RingGeometry( this.radius*1.1, this.radius*1.3, 32 );
+		var ringMaterial = new THREE.MeshLambertMaterial( { color: 0xffff00, side: THREE.DoubleSide, map: new THREE.ImageUtils.loadTexture('maps/sun.jpg')} );
+		var ringMesh = new THREE.Mesh( ringGeo, ringMaterial );
+		this.ring = ringMesh;	
+		this.scene.add( this.ring );   
+		console.log("added ring");
+		this.ring.castShadow = true;
+		this.ring.receiveShadow = true;
+		
+		this.ring.rotation.x = myDegToRad(60);
 	}
 
 	this.doMoon = function(_moonsNum){
@@ -54,39 +86,13 @@ function planet(_sceneObject,radius){
 			this.moons[i].receiveShadow = true;
 			this.scene.add(this.moons[i]);
 			console.log("added a moon");
-
-			moonsPivots[i] = new THREE.Object3D();
-			this.scene.add(moonsPivots[i]);
-			moonsPivots[i].add(this.moons[i]);
-
-			//// random orbit inclination
-		  	 // moonsPivots[i].rotation.x = myDegToRad((Math.random()*2-1)*30);
-		  	 // this.moons[i].updateMatrixWorld();			
-
-			
+	
 
 		}
 
 	}
 	this.initGeometry = function(){
 
-		// var moonGeo = new THREE.SphereGeometry(this.radius * 0.2,20,20);
-		// var moonMaterial= new THREE.MeshPhongMaterial({
-		// 	map : new THREE.ImageUtils.loadTexture("maps/moon.jpg"),
-		// 	specular : 0x010101,
-		// 	specularMap: new THREE.ImageUtils.loadTexture("maps/moon.jpg"),
-		// 	bumpScale : 0.01,
-		// 	bumpMap: new THREE.ImageUtils.loadTexture("maps/moon.jpg")
-		// });
-		// moonMesh = new THREE.Mesh(moonGeo, moonMaterial);
-		// moonMesh.castShadow = true;
-		// moonMesh.receiveShadow = true;
-		// this.scene.add(moonPivot);
-		// this.scene.add(moonMesh);
-		// this.moon = moonMesh;
-		// moonMesh.position.x = -4;
-
-		// moonPivot.add(moonMesh);
 
 
 
@@ -130,17 +136,23 @@ function planet(_sceneObject,radius){
 
 
 		var delta = clock.getDelta();
-		var orbitSpeed = clock.getElapsedTime() * 10;
-		var orbitRadius = 10;
+		var orbitSpeed = clock.getElapsedTime() * this.orbitSpeed;
+		//var orbitRadius = 10;
 	
 		this.planet.rotation.y += delta* 0.2;
 
 		this.planet.position.set(
-			(Math.sin(myDegToRad(orbitSpeed))* orbitRadius),
+			(Math.sin(myDegToRad(orbitSpeed))* this.orbitRadius),
 			0,
-			(Math.cos(myDegToRad(orbitSpeed))* orbitRadius)
-			);		
+			(Math.cos(myDegToRad(orbitSpeed))* this.orbitRadius)
+		);		
 
+		
+		if(this.bDoRing == true){
+		
+			// console.log(this.ring);
+			 this.ring.position.set(this.planet.position.x,this.planet.position.y,this.planet.position.z);
+		}
 
 			//////////////////////
 			////// moons orbits setup
@@ -158,7 +170,7 @@ function planet(_sceneObject,radius){
 
 			var randOrbitRadiusMult = 1+((rng.random(1, 1000) / 1000.0)*3);
 			var moonOrbitRadius = this.radius * 1.5  *randOrbitRadiusMult;
-			var moonOrbitSpeed = clock.getElapsedTime() * (rng.random(500, 1000) / 1000.0)*50;
+			var moonOrbitSpeed = clock.getElapsedTime() * (rng.random(500, 1000) / 1000.0)*80 *moonsSpeedMullt;
 			this.moons[i].position.set(
 				(Math.sin(myDegToRad(moonOrbitSpeed))* moonOrbitRadius) + this.planet.position.x,
 				(Math.sin(myDegToRad(moonOrbitSpeed))* moonOrbitRadius*0.2) + this.planet.position.y,
--- solar_system_scene_01.js
+++ solar_system_scene_01.js
@@ -10,9 +10,10 @@ for(var i =0 ; i< 10; i++){
 
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var controls = new THREE.FirstPersonControls(camera);
+controls.movementSpeed = 50.0;
 
 // var controls = new THREE.FirstPersonControls(camera);
 var sceneClock = new THREE.Clock({autoStrart: true});
@@ -97,40 +98,6 @@ sound.panner.connect(mainVolume);
 
 
 
-// var light= new THREE.DirectionalLight(0xffffff);
-var spotLight= new THREE.SpotLight(0xffffff,2,100,myDegToRad(30),1);
-
-
-
-spotLight.position.set(0,0,0);
-spotLight.target.position.set(10,0,10);
-
-
-spotLight.castShadow = true;
-spotLight.onlyShadow = true;
-spotLight.shadowBias = -0.0001
-
-spotLight.shadowMapWidth = 2048;
-spotLight.shadowMapHeight = 2048;
-spotLight.shadowDarkness = 0.9;
-
-// light.shadowMapDebug = true;
-
-spotLight.shadowCameraNear = 1;
-spotLight.shadowCameraFar = 500;
-spotLight.shadowCameraFov =60;
-// spotLight.shadowCameraVisible = true;
-
-
-scene.add(spotLight);
-scene.add(spotLight.target);
-
-
-
-
-
-
-
 
 
 var sunLight = new THREE.PointLight(0xffffff,1.8);
@@ -139,35 +106,104 @@ sunLight.position.set(0,0,0);
 scene.add(sunLight);
 
 
+var sunGeo = new THREE.SphereGeometry(3,30,30);
+var sunMaterial = new THREE.MeshBasicMaterial({map:new THREE.ImageUtils.loadTexture('maps/sun.jpg')});
+var sun = new THREE.Mesh(sunGeo, sunMaterial);
+
+scene.add(sun);
+
+
+var speedMult = 0.01;
+var planetsArray = [];
+
+
+var mercury = new planet(scene,1);
+mercury.setTexture(new THREE.ImageUtils.loadTexture("maps/mercury.jpg"));
+mercury.setOrbitRadius(10);
+mercury.setOrbitSpeed(50.55 * speedMult)
+planetsArray[0] = mercury;
+
+var venus = new planet(scene,0.8);
+venus.setTexture(new THREE.ImageUtils.loadTexture("maps/venus.jpg"));
+venus.setOrbitRadius(15);
+venus.setOrbitSpeed(32.5 * speedMult)
+planetsArray[1] = venus;
+
+var earth = new planet(scene,2);
+earth.setTexture(new THREE.ImageUtils.loadTexture("maps/earth.jpg"));
+earth.setOrbitRadius(25);
+earth.setOrbitSpeed(45.8 * speedMult)
+earth.doMoon(1);
+planetsArray[2] = earth;
+
+var mars = new planet(scene,1.2);
+mars.setTexture(new THREE.ImageUtils.loadTexture("maps/mars.jpg"));
+mars.setOrbitRadius(40);
+mars.setOrbitSpeed(39.9 * speedMult);
+mars.doMoon(2)
+planetsArray[3] = mars;
+
+var jupiter = new planet(scene,4);
+jupiter.setTexture(new THREE.ImageUtils.loadTexture("maps/jupiter.jpg"));
+jupiter.setOrbitRadius(60);
+jupiter.setOrbitSpeed(50.1 * speedMult);
+jupiter.doMoon(2)
+planetsArray[4] = jupiter;
+
+var saturn = new planet(scene,3);
+saturn.setTexture(new THREE.ImageUtils.loadTexture("maps/saturn.jpg"));
+saturn.setOrbitRadius(80);
+saturn.setOrbitSpeed(49.2 * speedMult);
+saturn.doRing();
+planetsArray[5] = saturn;
+
+
+
+
+//// planets spot lights for shadow mapping
+
+var spotLightsArray = [];
+for(var i=0; i < planetsArray.length; i++){
+
+	spotLightsArray[i] = new THREE.SpotLight(0xffffff,10,100,myDegToRad(30),1);
+	//var mercurySpotLight= new THREE.SpotLight(0xffffff,2,100,myDegToRad(30),1);
+	spotLightsArray[i].position.set(0,0,0);
+	spotLightsArray[i].castShadow = true;
+	spotLightsArray[i].onlyShadow = true;   //// <------------ only shadows
+	spotLightsArray[i].shadowBias = -0.0001
+	spotLightsArray[i].shadowMapWidth = 1024;
+	spotLightsArray[i].shadowMapHeight = 1024;
+	spotLightsArray[i].shadowDarkness = 0.9;
+	spotLightsArray[i].shadowCameraNear = 1;
+	spotLightsArray[i].shadowCameraFar = 500;
+	spotLightsArray[i].shadowCameraFov =30;
+	// spotLightsArray[i].shadowCameraVisible = true;
+	scene.add(spotLightsArray[i]);
+	scene.add(spotLightsArray[i].target);
+}
+
+
+
+
+
+///////////////////
+/////////// END planets spotlights
+/////////////////////////:
 
 
 
 
 
 
-
-
-
-var myPlanet = new planet(scene,2);
-myPlanet.doMoon(1);
-
-// myPlanet.moon.castShadow = true;
-// myPlanet.moon.receiveShadow = true;
-
-// myPlanet.planetMesh.castShadow = true;
-// myPlanet.planetMesh.receiveShadow = true;
-
-
-
-
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 renderer.shadowMapEnabled = true;
+
 renderer.shadowMapType = THREE.PCFSoftShadowMap;
 
 // renderer.shadowMapCullFace = THREE.CullFaceFront;
-// spotLight.shadowCascadeCount = 3;
+// earthSpotLight.shadowCascadeCount = 3;
 // renderer.shadowMapCascade = true;
 
 camera.position.y = 20;
@@ -186,7 +222,7 @@ camera.position.z = 20;
 
 // 	uniforms.texture1.value.wrapS = uniforms.texture1.value.wrapT = THREE.RepeatWrapping;
 
-// myPlanet.planetMesh.material = 	new THREE.ShaderMaterial( {
+// earth.planetMesh.material = 	new THREE.ShaderMaterial( {
 
 // 					uniforms: uniforms,
 // 					transparent : true,
@@ -195,9 +231,9 @@ camera.position.z = 20;
 // 					fragmentShader: document.getElementById( 'fragmentShader' ).textContent
 
 // 				} );
-// console.log(myPlanet.planetMesh.material.uniforms);
+// console.log(earth.planetMesh.material.uniforms);
 
-//  myPlanet.planetMesh.material.uniforms.sunPosition.needsUpdate = true;
+//  earth.planetMesh.material.uniforms.sunPosition.needsUpdate = true;
 ////////////////////
 /////////// end NIGHT SHADER TEST
 //////////////////
@@ -209,9 +245,10 @@ var once = 0;
 function animate(t){
 
 
+		for(var i=0; i<spotLightsArray.length; i++){
+			spotLightsArray[i].target.position.set(planetsArray[i].planet.position.x , planetsArray[i].planet.position.y , planetsArray[i].planet.position.z);
+		}
 
-		
-		spotLight.target.position.set(myPlanet.planet.position.x , myPlanet.planet.position.y , myPlanet.planet.position.z);
 		var dt = sceneClock.getDelta();
 
 		controls.update(dt);
@@ -220,12 +257,16 @@ function animate(t){
 
 		var p = new THREE.Vector3();
 
-		p.setFromMatrixPosition(myPlanet.moons[0].matrixWorld);
+		p.setFromMatrixPosition(earth.moons[0].matrixWorld);
 		var px = p.x, py = p.y, pz = p.z;
 
 		// /////
+		for (var i = planetsArray.length - 1; i >= 0; i--) {
+			planetsArray[i].update();
 
-		myPlanet.update();
+		};
+
+
 
 		// /////
 
@@ -237,16 +278,16 @@ function animate(t){
 
 
 
-		var volumeVal = (1-Math.sqrt(Math.clamp(camera.position.distanceTo(myPlanet.moons[0].position)/9,0,1)))*0.2;
+		var volumeVal = (1-Math.sqrt(Math.clamp(camera.position.distanceTo(earth.moons[0].position)/9,0,1)))*0.2;
 		mainVolume.gain.value = volumeVal;
 		// console.log(volumeVal);
 
 
 
-		 myPlanet.moons[0].updateMatrixWorld();
-		 // console.log(myPlanet.moon.position.x);
+		 earth.moons[0].updateMatrixWorld();
+		 // console.log(earth.moon.position.x);
 		var p = new THREE.Vector3();
-		p.setFromMatrixPosition(myPlanet.moons[0].matrixWorld);
+		p.setFromMatrixPosition(earth.moons[0].matrixWorld);
 
 		// And copy the position over to the sound of the object.
 		sound.panner.setPosition(p.x * mult, p.y * mult, p.z * mult);
@@ -262,20 +303,20 @@ function animate(t){
 
 
 
-		myPlanet.moons[0].updateMatrixWorld();
+		earth.moons[0].updateMatrixWorld();
 
 		var q = new THREE.Vector3();
-		q.setFromMatrixPosition(myPlanet.moons[0].matrixWorld);
+		q.setFromMatrixPosition(earth.moons[0].matrixWorld);
 		var dx = q.x-px, dy = q.y-py, dz = q.z-pz;
 
 		sound.panner.setPosition(q.x * mult, q.y* mult, q.z* mult);
 		sound.panner.setVelocity(dx/dt, dy/dt, dz/dt);		
 		
 
-		//renderer.setClearColor( 0x000000 );
+		renderer.setClearColor( 0x000000);
 
-    renderer.render(scene, camera);
-    window.requestAnimationFrame(animate, renderer.domElement);		
+	    renderer.render(scene, camera);
+	    window.requestAnimationFrame(animate, renderer.domElement);		
 }
 
 animate(new Date().getTime());
